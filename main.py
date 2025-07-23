55667e76b3a95f3a1d8c644dbf8802d6c9bff9db
kodunaiff <kodunaiff@gmail.com>
2025-07-23 13:07:44 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+
+from api_v1 import router as router_v1
+from core.config import settings
+from core.models import Base, db_helper
+from items_views import router as items_router
+from users.views import router as users_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(items_router)
+app.include_router(users_router)
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
+
+
+@app.get("/")
+def root():
+    return {"message": "ok"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
