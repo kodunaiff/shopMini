28dd916e872c3eebb4cb8f8ef143582815c4b42e
kodunaiff <kodunaiff@gmail.com>
2025-07-30 00:58:43 +0800

    начал работу над jwt token

---
+++ api_v1/demo_auth/demo_jwt_auth.py
@@ -0,0 +1,74 @@
+from fastapi import APIRouter, Depends, Form, HTTPException, status
+from pydantic import BaseModel
+
+from auth import utils as auth_utils
+from users.schemas import UserSchema
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    token_type: str
+
+
+router = APIRouter(prefix="/jwt", tags=["JWT"])
+
+john = UserSchema(
+    username="john",
+    password=auth_utils.hash_password("qwerty"),
+    email="john@example.com",
+)
+
+sam = UserSchema(
+    username="sam",
+    password=auth_utils.hash_password("secret"),
+)
+
+users_db: dict[str, UserSchema] = {
+    john.username: john,
+    sam.username: sam,
+}
+
+
+def validate_auth_user(
+    username: str = Form(),
+    password: str = Form(),
+):
+    unauthed_exc = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="invalid username or password"
+    )
+    if not (user := users_db.get(username)):
+        raise unauthed_exc
+
+    if not auth_utils.validate_password(
+        password=password,
+        hashed_password=user.password,
+    ):
+        raise unauthed_exc
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="user inactive",
+        )
+
+    return user
+
+
+
+
+@router.post("/login/", response_model=TokenInfo)
+def auth_user_issue_jwt(
+    user: UserSchema = Depends(validate_auth_user),
+):
+    jwt_payload = {
+        # subject
+        "sub": user.username,
+        "username": user.username,
+        "email": user.email,
+        # "logged_in_at"
+    }
+    token = auth_utils.encode_jwt(jwt_payload)
+    return TokenInfo(
+        access_token=token,
+        token_type="Bearer",
+    )
